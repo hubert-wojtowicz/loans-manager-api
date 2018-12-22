7fbb97be373b8ec64404e35af6099f1637887192
Hubert Wójtowicz <hubert.karol.wojtowicz@gmail.com>
2018-12-22 12:36:42 +0100

    Style: AuthenticationFailed comunicat corrected.

--- LoansManager/Resources/UserControllerResources.cs
+++ LoansManager/Resources/UserControllerResources.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace LoansManager.Resources
+﻿namespace LoansManager.Resources
 {
     public static class UserControllerResources
     {
-        public const string AuthenticationFailed = "Login or password invalid";
+        public const string AuthenticationFailed = "Login or password is invalid";
         public const string MaxNumberOfRecordToGetExceeded = "To many records requested.";
     }
 }
