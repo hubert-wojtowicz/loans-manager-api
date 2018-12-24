6b258bb8cfc5b789828a63fdcab05d03f0625fe1
Hubert Wójtowicz <hubert.karol.wojtowicz@gmail.com>
2018-12-24 22:47:41 +0100

    Fix: Missing repaid date filled.

--- LoansManager.Services/Implementations/CommandHandlers/RepayLoanCommandHandler.cs
+++ LoansManager.Services/Implementations/CommandHandlers/RepayLoanCommandHandler.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using LoansManager.DAL.Repositories.Interfaces;
 using LoansManager.Services.Commands;
 using LoansManager.Services.Infrastructure.CommandsSetup;
@@ -18,6 +19,7 @@ namespace LoansManager.Services.Implementations.CommandHandlers
         {
             var loan = await loansRepository.GetAsync(command.LoanId);
             loan.IsRepaid = true;
+            loan.RepaidDate = DateTime.UtcNow;
             await loansRepository.UpdateAysnc(loan);
         }
     }
